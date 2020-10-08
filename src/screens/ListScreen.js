2530ed3c24b4b3784339165d0269a2dc8012c4ed
naveendk <naveenkolambage@gmail.com>
2020-10-08 21:57:35 +1300

    added keyExtractor to get unique keys

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,21 +4,22 @@ import {View, Text, StyleSheet, FlatList} from 'react-native'
 const ListScreen = () => {
     
     const friends =[
-        {name:"Friend 1"},
-        {name:"Friend 2"},
-        {name: "Friend 3"},
-        {name:"Friend 4"},
-        {name:"Friend 5"},
-        {name:"Friend 6"},
-        {name: "Friend 7"},
-        {name:"Friend 8"}
+        {name:"Friend 1" , age: "20" },
+        {name:"Friend 2", age: "45"},
+        {name: "Friend 3" , age: "32"},
+        {name:"Friend 4", age: "27"},
+        {name:"Friend 5", age: "43"},
+        {name:"Friend 6",  age: "23"},
+        {name: "Friend 7",age: "29"},
+        {name:"Friend 8",age: "09"}
     ]
 
     return (
     <FlatList 
-    data= {friends} renderItem={({item})=>{
+     keyExtractor = {friend=>friend.name}
+     data= {friends} renderItem={({item})=>{
         //element ==={item:{name:'Friend 1'}, index:0} therefore we use destructure to get item out
-    return <Text>{item.name}</Text>
+          return <Text>{item.name} - {item.age}</Text>
 
     }}
     />
